198357728682bfcf62712b8f7965473d1c392ad3
Jon Seymour <jon.seymour@gmail.com>
2011-04-22 13:26:48 +1000

    Updated example to indicate which stream output is expected on.

--- example/example.js
+++ example/example.js
@@ -1,29 +1,29 @@
 var idiomatic=require("idiomatic-stdio");
 
-console.log("by default, console.log() writes to process.stdout");
-console.info("console.info() does too");
+console.log("stdout: by default, console.log() writes to process.stdout");
+console.info("stdout: console.info() does too");
 
-console.log('shortly, we will call require("idiomatic-stdio").rebind() to redirect console.log() output process.stderr');
+console.log('stdout: shortly, we will call require("idiomatic-stdio").rebind() to redirect console.log() output process.stderr');
 
 idiomatic.rebind();
-console.log('see mum, console.log() now writes to stderr');
-console.info('and console.info() does too');
+console.log('stderr: see mum, console.log() now writes to stderr');
+console.info('stderr: and console.info() does too');
 idiomatic.unbind();
-console.log('but unbind() undoes the effect of rebind on console.log()');
-console.info('and console.info() too');
+console.log('stdout: but unbind() undoes the effect of rebind on console.log()');
+console.info('stdout: and console.info() too');
 
 var api={
     foo: function() {
-	console.log("api.foo expects to console.log to write to stdout, so let it");
+	console.log("stdout: api.foo expects to console.log to write to stdout, so let it");
     }
 };
 
 idiomatic.with(function() {
-    console.log('with() can be used to rebind console.log() for the duration of a function call');
-    console.info('and console.info() too');
+    console.log('stderr: with() can be used to rebind console.log() for the duration of a function call');
+    console.info('stderr: and console.info() too');
 });
 
-console.log("after with(), console.log() should write to stdout again");
+console.log("stdout: after with(), console.log() should write to stdout again");
 
 
 idiomatic.rebind();
@@ -34,4 +34,4 @@ idiomatic.unbind();
 idiomatic
     .console
     .options({ idiom: 'diagnostics' })
-    .log('this is an example of a suggested idiom for specfifying how the API is to be used');
+    .log('stderr: this is an example of a suggested idiom for specfifying how the API is to be used');
